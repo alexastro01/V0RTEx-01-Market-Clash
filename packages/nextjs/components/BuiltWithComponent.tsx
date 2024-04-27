b6b0da95126db9981066d90d1e76c647d903b34e
Alex Astro <101653301+alexastro01@users.noreply.github.com>
2024-04-27 17:47:47 +0300

    remove image

--- packages/nextjs/components/BuiltWithComponent.tsx
+++ packages/nextjs/components/BuiltWithComponent.tsx
@@ -5,9 +5,8 @@ const BuiltWithComponent = () => {
   return (
     <div>
         <h2 className='text-center text-sm pt-12 pb-4'>Built with</h2>
-        <div className='grid grid-cols-3 items-center justify-items-center'>
+        <div className='grid grid-cols-2 items-center justify-items-center'>
             <Image src={'/scroll.jpeg'} width={"84"} height={"84"} alt="scroll logo" className='rounded-full' />
-            <Image src={'/Covalent.png'} width={"84"} height={"84"} alt="Covalent logo" />
             <Image src={'/Chainlink.png'} width={"84"} height={"84"} alt="Chainlink logo" />
         </div>
     </div>
